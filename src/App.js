11c702f72a041c757dfd08262ad02a0b6bfc6c75
Primadika Dwijantara <primadika.dwijantara@gmail.com>
2018-02-23 18:07:19 +0700

    adding flow for typechecking in babel and eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react"
+// @flow
+
+import React from "react"
 import "./App.scss"
 
-export default class App extends Component {
-	render() {
-		return (
-			<div className="hello">Hello world!</div>
-		)
-	}
-}
+const App = () => (
+	<div className="hello">Hello world!</div>
+)
+
+export default App
